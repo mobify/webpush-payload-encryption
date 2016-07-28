4f72269fba883338ebdd8cdc7acfe23e862f446c
Ben Last <ben@benlast.com>
2016-07-28 12:39:15 -0700

    Work-in-progress

--- webpush_encryption/__init__.py
+++ webpush_encryption/__init__.py
@@ -13,6 +13,7 @@ import OpenSSL
 
 
 CONTENT_APPLICATION_OCTET_STREAM = 'application/octet-stream'
+DEFAULT_BACKEND = default_backend()
 HEADER_CONTENT_ENCODING = 'Content-Encoding'
 HEADER_CONTENT_LENGTH = 'Content-Length'
 HEADER_CONTENT_TYPE = 'Content-Type'
@@ -23,9 +24,13 @@ HEADER_ENCRYPTION = 'Encryption'
 HEADER_ENCRYPTION_KEY = 'Encryption-Key'
 
 
-def encrypt_gcm(key, nonce, plaintext):
+def encrypt_plaintext(key, nonce, plaintext):
     """
     Encrypts the given plaintext with the given AES key in GCM mode.
+
+    In this context, GCM is Galois Counter Mode. See
+    https://cryptography.io/en/latest/hazmat/primitives/symmetric-encryption/?highlight=cipher#cryptography.hazmat.primitives.ciphers.modes.GCM
+
     Uses the provided nonce to initialize the encryption.
     :returns: the output ciphertext as a string
     """
@@ -33,7 +38,7 @@ def encrypt_gcm(key, nonce, plaintext):
     encryptor = Cipher(
         algorithms.AES(key),
         modes.GCM(nonce),
-        backend=default_backend()
+        backend=DEFAULT_BACKEND
     ).encryptor()
 
     ciphertext = encryptor.update('\x00\x00' + plaintext) + encryptor.finalize()
@@ -44,12 +49,12 @@ def encrypt_gcm(key, nonce, plaintext):
 
 # pylint: disable=invalid-name
 def HKDF_256(**kwargs):
-    return HKDF(algorithm=hashes.SHA256(), backend=default_backend(), **kwargs)
+    return HKDF(algorithm=hashes.SHA256(), backend=DEFAULT_BACKEND, **kwargs)
 
 
-def chrome_auth_info(info_type, client_key, server_key):
+def encode_auth_info(info_type, client_key, server_key):
     return '\0'.join((
-        'Content-Encoding: %s' % info_type,
+        '%s: %s' % (HEADER_CONTENT_ENCODING, info_type),
         'P-256',
         (
             struct.pack('>H', len(client_key)) + client_key +
@@ -87,15 +92,21 @@ def encrypt_encoded_payload(key, payload, auth=None):
     :param auth: The auth secret as raw bytes
     :returns ciphertext string, header dict
     """
+
     # KEY EXCHANGE
-    server_private_key = ec.generate_private_key(ec.SECP256R1(), default_backend())
+    server_private_key = ec.generate_private_key(
+        ec.SECP256R1(),
+        DEFAULT_BACKEND
+    )
+
     server_public_key = server_private_key.public_key().public_numbers().encode_point()
 
-    # This will throw ValueError if the key is invalid. We will handle
-    # this in the caller.
+    # This will throw ValueError if the key is invalid.
     client_public_key = ec.EllipticCurvePublicNumbers.from_encoded_point(
         ec.SECP256R1(), key,
-    ).public_key(default_backend())
+    ).public_key(
+        DEFAULT_BACKEND
+    )
 
     shared_secret = server_private_key.exchange(ec.ECDH(), client_public_key)
 
@@ -116,7 +127,7 @@ def encrypt_encoded_payload(key, payload, auth=None):
 
     key_info = 'Content-Encoding: aesgcm128'
     if auth is not None:
-        key_info = chrome_auth_info(
+        key_info = encode_auth_info(
             info_type='aesgcm',
             client_key=key,
             server_key=server_public_key
@@ -129,11 +140,12 @@ def encrypt_encoded_payload(key, payload, auth=None):
 
     nonce_info = 'Content-Encoding: nonce'
     if auth is not None:
-        nonce_info = chrome_auth_info(
+        nonce_info = encode_auth_info(
             info_type='nonce',
             client_key=key,
             server_key=server_public_key
         )
+
     nonce = HKDF_256(
         length=12,
         salt=salt,
@@ -142,7 +154,7 @@ def encrypt_encoded_payload(key, payload, auth=None):
 
     # ENCRYPTION
 
-    ciphertext = encrypt_gcm(aes_key, nonce, payload)
+    ciphertext = encrypt_plaintext(aes_key, nonce, payload)
 
     # JR Conlin's example page (https://jrconlin.github.io/WebPushDataTestPage/)
     # shows the following headers:
@@ -163,12 +175,6 @@ def encrypt_encoded_payload(key, payload, auth=None):
         # See https://tools.ietf.org/html/draft-ietf-webpush-encryption
         HEADER_CONTENT_LENGTH: len(ciphertext),
         HEADER_CONTENT_TYPE: CONTENT_APPLICATION_OCTET_STREAM,
-
-        # https://developers.google.com/web/updates/2016/03/web-push-encryption
-        # suggests we use Crypto-Key with 'aesgcm' (not 'aesgcm128'). The
-        # Mozilla servers return a 400 stating that we're using an obsolete
-        # protocol if we use Crypto-Key with 'aesgcm128'. So we changed to
-        # use 'aesgcm'.
         HEADER_CONTENT_ENCODING: content_encoding,
         crypto_key_header: 'keyid=p256dh;dh=%s' % header_encode(server_public_key),
         HEADER_ENCRYPTION: 'keyid=p256dh;salt=%s' % header_encode(salt)
--- webpush_encryption/test/tests.py
+++ webpush_encryption/test/tests.py
@@ -1,31 +1,65 @@
 # -*- coding: utf-8 -*-
-import mock
+import base64
 from unittest import TestCase
 
-from .. import *
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives.ciphers import (
+    Cipher, algorithms, modes
+)
+import mock
+
+from .. import (
+    encrypt_payload,
+    DEFAULT_BACKEND,
+    HEADER_CRYPTO_KEY,
+    HEADER_ENCRYPTION_KEY,
+    HEADER_ENCRYPTION,
+    HKDF_256
+)
 
 
 class TestEncryption(TestCase):
 
-    KEY = 'BBlN5VUVJAx3fIRwhSdCxXgJF2egGZkDJAAqZ1Ao20chG' \
-          '/O0uaM91twU8a8aPWP7fMAePomPY6icfuzocGGNR3g='
+    PUBLIC_KEY = None
+    PRIVATE_KEY = None
+    KEY_B64 = None
 
-    AUTH = 'Or79lW+u11VPfLG83mxBfQ=='
+    AUTH_B64 = 'Or79lW+u11VPfLG83mxBfQ=='
+
+    SALT = '\xbaa\x1c\xd00w\xb6\xb3\xb6\xbdh~\x8e8\x10|'
 
     PLAINTEXT = {
         'abc': 123,
         'def': 456
     }
 
+    @classmethod
+    def setUpClass(cls):
+        # See https://tools.ietf.org/html/draft-ietf-webpush-encryption#section-5
+        # for the specification of the key.
+        cls.PRIVATE_KEY = ec.generate_private_key(
+            ec.SECP256R1,
+            DEFAULT_BACKEND
+        )
+
+        cls.PUBLIC_KEY = (
+            cls.PRIVATE_KEY
+            .public_key()
+            .public_numbers()
+            .encode_point()
+        )
+
+        cls.KEY_B64 = base64.b64encode(cls.PUBLIC_KEY)
+
     def test_payload_encryption(self):
 
         with mock.patch(
             'OpenSSL.rand.bytes',
-            new=lambda x: '\xbaa\x1c\xd00w\xb6\xb3\xb6\xbdh~\x8e8\x10|'
+            new=lambda x: self.SALT
         ):
             ciphertext, headers = encrypt_payload(
-                key=self.KEY,
-                auth=self.AUTH,
+                key=self.KEY_B64,
+                auth=self.AUTH_B64,
                 payload=self.PLAINTEXT
             )
 
@@ -42,7 +76,7 @@ class TestEncryption(TestCase):
             # Verify that omitting the auth returns the alternate
             # headers
             ciphertext, headers = encrypt_payload(
-                key=self.KEY,
+                key=self.KEY_B64,
                 auth=None,
                 payload=self.PLAINTEXT
             )
